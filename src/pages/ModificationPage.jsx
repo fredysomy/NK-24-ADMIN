d6475259dc61e85f0f1e365dcdda93144736dc81
Basil Rari <basilrari@gmail.com>
2024-02-24 17:08:00 +0530

    Added Modification Page Firebase Connection

--- src/pages/ModificationPage.jsx
+++ src/pages/ModificationPage.jsx
@@ -1,7 +1,12 @@
 import React, { useState, useEffect } from "react";
 import { db } from "../firebase/firebase";
 import { useNavigate } from "react-router-dom";
-import { doc, getDoc, updateDoc } from "firebase/firestore";
+import {
+  QueryCompositeFilterConstraint,
+  doc,
+  getDoc,
+  updateDoc,
+} from "firebase/firestore";
 import { collection, query, where, getDocs } from "firebase/firestore";
 
 function RegistrationQuery() {
@@ -13,35 +18,38 @@ function RegistrationQuery() {
   const [registrationData, setRegistrationData] = useState(null);
   const [team, setTeam] = useState(null);
   const [selectedField, setSelectedField] = useState("attended");
-  const [selectedFields, setSelectedFields] = useState([]);
   const [newValue, setNewValues] = useState("");
 
-  // ... (existing functions)
   const navigate = useNavigate();
-  
-  
+
   const navigateTo = (path) => {
     alert("Update successful");
     navigate(path);
-    
   };
-  const handleFieldsSelectChange = (e) => {
-    const selectedOptions = Array.from(e.target.selectedOptions, (option) => option.value);
-    setSelectedFields(selectedOptions);
-  };
+
   const [dropdownClicked, setDropdownClicked] = useState(false);
   const handleDropdownClick = () => {
     setDropdownClicked(true);
   };
 
-  const handleNewValueChange = (field, value) => {
+  let globalValue = ""; // Define global variable for value
+  let globalSelectedValue = ""; // Define global variable for selected value
+  let globalNkid = "";
+
+  const handleNewValueChange = (e) => {
+    const value = e.target.value;
     setNewValues(value);
-    console.log(newValue);
-}
-  
-  const handleFieldSelectChange = (e) => {
-    setSelectedField(e.target.value);
+    globalValue = value; // Update global value variable
+    console.log("Global V ", globalValue);
   };
+
+  const handleFieldSelectChange = (e) => {
+    const selectedValue = e.target.value;
+    setSelectedField(selectedValue);
+    globalSelectedValue = selectedValue;
+    console.log("Global SV ", globalSelectedValue); // Update global selected value variable
+  };
+
   useEffect(() => {
     if (details && details.hasOwnProperty("attended")) {
       setAttended(details.attended);
@@ -50,14 +58,6 @@ function RegistrationQuery() {
 
   const handleInputChange = (e) => {
     setRegistrationId(e.target.value);
-  };
-
-  const handleCheckboxChange = async (e) => {
-    const newAttended = e.target.checked;
-    setAttended(newAttended);
-    if (details) {
-      await updateFirestoreAttended(newAttended);
-    }
   };
 
   const handleSubmit = async (e) => {
@@ -69,15 +69,12 @@ function RegistrationQuery() {
   };
 
   const fetchDetails = async () => {
-    console.log("Fetching details...");
     const docRef = doc(db, "Registrations", nkid);
     const docSnap = await getDoc(docRef);
 
     if (docSnap.exists()) {
-      console.log("Registration details found:", docSnap.data());
       setDetails(docSnap.data());
       await fetchUserDetails(docSnap.data().nkid);
-      console.log("This is docsnap", docSnap.data().nkid);
       await fetchRegistrationData(docSnap.data().nkid); // Fetch registration data
     } else {
       alert("No such document!");
@@ -86,15 +83,14 @@ function RegistrationQuery() {
   };
 
   const fetchUserDetails = async (nkid) => {
-    console.log("Fetching user details for NKID:", nkid);
     try {
       const usersRef = collection(db, "users");
       const q = query(usersRef, where("NKID", "==", nkid));
       const querySnapshot = await getDocs(q);
-      console.log(querySnapshot);
+      globalNkid = nkid;
+      console.log("This is inside fetchUserDetails : ",globalNkid);
 
       querySnapshot.forEach((doc) => {
-        console.log("User details found:", doc.data());
         setUserDetails(doc.data());
       });
 
@@ -134,16 +130,34 @@ function RegistrationQuery() {
     }
   };
 
-  const updateFirestoreAttended = async (newAttended) => {
-    const docRef = doc(db, "Registrations", nkid);
+  const updateUser2 = async (globalNkid, globalSelectedValue, globalValue) => {
     try {
-      await updateDoc(docRef, {
-        attended: newAttended,
-      });
-      alert("Attendance updated successfully.");
+      console.log("This is Global Selected Value : ", globalSelectedValue);
+      console.log("This is Global Value : ", globalValue);
+      console.log("This is global : ", globalNkid.substring(0, 8));
+
+      const user2RefQuery = query(
+        collection(db, "users2test"),
+        where("NKID", "==", globalNkid.substring(0, 8))
+      );
+      const querySnapshot = await getDocs(user2RefQuery);
+
+      if (!querySnapshot.empty) {
+        const docSnapshot = querySnapshot.docs[0]; // Assuming there's only one matching document
+        const user2Ref = doc(db, "users2test", docSnapshot.id);
+
+        console.log("Updating field:", globalSelectedValue);
+        console.log("New value:", globalValue);
+
+        await updateDoc(user2Ref, {
+          [globalSelectedValue]: globalValue, // Update the selected field with the global value
+        });
+        console.log("User2 updated successfully!");
+      } else {
+        console.log("No matching document found in user2 collection");
+      }
     } catch (error) {
-      console.error("Error updating document:", error);
-      alert("Failed to update the document.");
+      console.error("Error updating user2:", error);
     }
   };
 
@@ -170,172 +184,158 @@ function RegistrationQuery() {
       </div>
 
       {details && userDetails && registrationData && (
-  <div>
-    <div className="px-5 mt-20 flex">
-      <div className="flex-1"></div>
-      {/* User Details Table */}
-      <div className="w-1/4 px-2 flex flex-col items-center">
-        <h3 className="font-bold mb-5 text-xl">User Details</h3>
-        <table className="border border-gray-200">
-          <tbody>
-            <tr>
-              <th className="border border-gray-200 p-2 text-left bg-gray-800">
-                Name
-              </th>
-              <td className="border border-gray-200 p-2 text-left bg-transparent">
-                {userDetails.name}
-              </td>
-            </tr>
-            <tr>
-              <th className="border border-gray-200 p-2 text-left bg-gray-800">
-                NKID
-              </th>
-              <td className="border border-gray-200 p-2 text-left bg-transparent">
-                {userDetails.NKID}
-              </td>
-            </tr>
-            <tr>
-              <th className="border border-gray-200 p-2 text-left bg-gray-800">
-                College
-              </th>
-              <td className="border border-gray-200 p-2 text-left bg-transparent">
-                {userDetails.college}
-              </td>
-            </tr>
-            <tr>
-              <th className="border border-gray-200 p-2 text-left bg-gray-800">
-                Branch
-              </th>
-              <td className="border border-gray-200 p-2 text-left bg-transparent">
-                {userDetails.branch}
-              </td>
-            </tr>
-            <tr>
-              <th className="border border-gray-200 p-2 text-left bg-gray-800">
-                Semester
-              </th>
-              <td className="border border-gray-200 p-2 text-left bg-transparent">
-                {userDetails.semester}
-              </td>
-            </tr>
-          </tbody>
-        </table>
-      </div>
-
-      {/* Registration Details Table */}
-      <div className="w-1/4 px-2 flex flex-col justify-center items-center">
-            <h3 className="font-bold mb-5 text-xl">Registration Details</h3>
-            <div className="flex justify-center items-center">
+        <div>
+          <div className="px-5 mt-20 flex">
+            <div className="flex-1"></div>
+            {/* User Details Table */}
+            <div className="w-1/4 px-2 flex flex-col items-center">
+              <h3 className="font-bold mb-5 text-xl">User Details</h3>
               <table className="border border-gray-200">
                 <tbody>
                   <tr>
                     <th className="border border-gray-200 p-2 text-left bg-gray-800">
-                      Attended
+                      Name
                     </th>
                     <td className="border border-gray-200 p-2 text-left bg-transparent">
-                      {attended ? "Yes" : "No"}
-                      <input
-                        type="checkbox"
-                        checked={attended}
-                        onChange={handleCheckboxChange}
-                        className="ml-2"
-                      />
+                      {userDetails.name}
                     </td>
                   </tr>
                   <tr>
                     <th className="border border-gray-200 p-2 text-left bg-gray-800">
-                      Event ID
+                      NKID
                     </th>
                     <td className="border border-gray-200 p-2 text-left bg-transparent">
-                      {registrationData.eventid}
+                      {userDetails.NKID}
                     </td>
                   </tr>
                   <tr>
                     <th className="border border-gray-200 p-2 text-left bg-gray-800">
-                      Event
+                      College
                     </th>
                     <td className="border border-gray-200 p-2 text-left bg-transparent">
-                      {registrationData.eventname}
+                      {userDetails.college}
                     </td>
                   </tr>
                   <tr>
                     <th className="border border-gray-200 p-2 text-left bg-gray-800">
-                      Payment ID
+                      Branch
                     </th>
                     <td className="border border-gray-200 p-2 text-left bg-transparent">
-                      {registrationData.payment_id}
+                      {userDetails.branch}
                     </td>
                   </tr>
-                  {team && ( // Only render this row if team is not null
-                    <tr>
-                      <th className="border border-gray-200 p-2 text-left bg-gray-800">
-                        Team
-                      </th>
-                      <td className="border border-gray-200 p-2 text-left bg-transparent">
-                        {team}
-                      </td>
-                    </tr>
-                  )}
+                  <tr>
+                    <th className="border border-gray-200 p-2 text-left bg-gray-800">
+                      Semester
+                    </th>
+                    <td className="border border-gray-200 p-2 text-left bg-transparent">
+                      {userDetails.semester}
+                    </td>
+                  </tr>
                 </tbody>
               </table>
             </div>
-          </div>
-          <div className="flex-1"></div>
-        </div>
 
-    {/* Dropdown to select field to modify */}
-    <div className="mt-10 flex flex-col justify-center alignitems-center flex flex-col items-center">
-      <h3 className="font-bold mb-5 text-xl">Select Field to Modify</h3>
-      <select
-        value={selectedField}
-        onChange={handleFieldSelectChange}
-        onClick={handleDropdownClick}
-        //multiple
-        className=" w-64 px-20 py-2 rounded-md border focus:outline-none bg-gray-800 text-white"
-      >
-        <option value="Name">Name</option>
-        <option value="NKID">NKID</option>
-        <option value="College">College</option>
-        <option value="Branch">Branch</option>
-        <option value="Semester">Semester</option>
-        <option value="Event ID">Event ID</option>
-        <option value="Event Name">Event Name</option>
-        <option value="Payment ID">Payment ID</option>
-        {/* Add more options for other fields as needed */}
-      </select>
-    </div>
-    {/* Input fields for modifying the selected fields */}
-    <div className=" px-2 flex flex-col justify-center items-center">
-      
-    {dropdownClicked && (
-        <>
-        <label htmlFor="newValue" className="py-5 font-bold mb-2 text-md text-white">
-          New Value:
-        </label>
-        <input
-          type="text"
-          id="newValue"
-          value={newValue}
-          onChange={handleNewValueChange}
-          className=" w-64 px-20 py-2 rounded-md border focus:outline-none bg-gray-800 text-white"
-      />
-      <div className=" w-64 px-20 py-5 rounded-md border:black focus:outline-none bg-black-800 text-white"
-     >
-      <button 
-      onClick={() => navigateTo("/selection")}
-      className="mt-2 px-4 py-2 rounded-md bg-blue-600 text-white hover:bg-blue-700 focus:outline-none focus:bg-blue-700"
-    >
-      Submit
-    </button>
-    </div>
-      </>
-    )}
-    </div> 
-    
-  </div>
-  
- )}
-    
+            {/* Registration Details Table */}
+            <div className="w-1/4 px-2 flex flex-col justify-center items-center">
+              <h3 className="font-bold mb-5 text-xl">Registration Details</h3>
+              <div className="flex justify-center items-center">
+                <table className="border border-gray-200">
+                  <tbody>
+                    <tr>
+                      <th className="border border-gray-200 p-2 text-left bg-gray-800">
+                        Event ID
+                      </th>
+                      <td className="border border-gray-200 p-2 text-left bg-transparent">
+                        {registrationData.eventid}
+                      </td>
+                    </tr>
+                    <tr>
+                      <th className="border border-gray-200 p-2 text-left bg-gray-800">
+                        Event
+                      </th>
+                      <td className="border border-gray-200 p-2 text-left bg-transparent">
+                        {registrationData.eventname}
+                      </td>
+                    </tr>
+                    <tr>
+                      <th className="border border-gray-200 p-2 text-left bg-gray-800">
+                        Payment ID
+                      </th>
+                      <td className="border border-gray-200 p-2 text-left bg-transparent">
+                        {registrationData.payment_id}
+                      </td>
+                    </tr>
+                    {team && ( // Only render this row if team is not null
+                      <tr>
+                        <th className="border border-gray-200 p-2 text-left bg-gray-800">
+                          Team
+                        </th>
+                        <td className="border border-gray-200 p-2 text-left bg-transparent">
+                          {team}
+                        </td>
+                      </tr>
+                    )}
+                  </tbody>
+                </table>
+              </div>
+            </div>
+            <div className="flex-1"></div>
+          </div>
+
+          {/* Dropdown to select field to modify */}
+          <div className="mt-10 flex flex-col justify-center alignitems-center flex flex-col items-center">
+            <h3 className="font-bold mb-5 text-xl">Select Field to Modify</h3>
+            <select
+              value={selectedField}
+              onChange={handleFieldSelectChange}
+              onClick={handleDropdownClick}
+              //multiple
+              className=" w-64 px-20 py-2 rounded-md border focus:outline-none bg-gray-800 text-white"
+            >
+              <option className="text-black" value="" disabled>
+                Select Event
+              </option>
+              <option value="name">Name</option>
+              <option value="email">Email</option>
+              <option value="phoneNumber">Phone Number</option>
+              <option value="college">College</option>
+              <option value="branch">Branch</option>
+              <option value="semester">Semester</option>
+            </select>
+          </div>
+          {/* Input fields for modifying the selected fields */}
+          <div className=" px-2 flex flex-col justify-center items-center">
+            {dropdownClicked && (
+              <>
+                <label
+                  htmlFor="newValue"
+                  className="py-5 font-bold mb-2 text-md text-white"
+                >
+                  New Value:
+                </label>
+                <input
+                  type="text"
+                  value={newValue}
+                  onChange={handleNewValueChange}
+                  className=" w-64 px-20 py-2 rounded-md border focus:outline-none bg-gray-800 text-white"
+                />
+                <div className=" w-64 px-20 py-5 rounded-md border:black focus:outline-none bg-black-800 text-white">
+                  <button
+                    onClick={async () => {
+                      await updateUser2(nkid,selectedField, newValue);
+                      navigateTo("/selection");
+                    }}
+                    className="mt-2 px-4 py-2 rounded-md bg-blue-600 text-white hover:bg-blue-700 focus:outline-none focus:bg-blue-700"
+                  >
+                    Submit
+                  </button>
+                </div>
+              </>
+            )}
+          </div>
+        </div>
+      )}
     </div>
   );
 }
